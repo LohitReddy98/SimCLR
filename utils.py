ac6af8f2adbd5b4bd9a628c6e43c91a0b73beed2
Chandra <lohitreddycht@gmail.com>
2023-11-04 22:46:07 -0700

    as

--- utils.py
+++ utils.py
@@ -28,6 +28,9 @@ class MNISTPair(MNIST):
         if img.shape[0] == 1:  # Grayscale image
             img = Image.fromarray(img[0], mode='L')  # Convert to a grayscale Image
             img = transforms.Grayscale(num_output_channels=3)(img)  # Convert to RGB
+            print(img.shape[0])
+            print(img)
+
 
         if self.transform is not None:
             pos_1 = self.transform(img)
